e56b636249399387a156b8829f4b23df4fe31ede
Limsungsucf19a <koko1967@naver.com>
2019-04-11 12:32:51 +0900

    Update 3번.py

--- 3번.py
+++ 3번.py
@@ -1,16 +1,20 @@
 # 소인수분해 프로그램
 
+a = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 x = int(input("인수분해할 숫자입력"))         
-d = 2                      
+d = 2   
+i = 0                   
 
 while d <= x:
     if x % d == 0:         
-        print(d)            
-        x = x / d   
+        a[i] = d           
+        x = x / d 
+        i = i + 1  
     else:
         d = d + 1  
-
-      
+       
+print(a)       
+print(max(a))  
 
 
 
