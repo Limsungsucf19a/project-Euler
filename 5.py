ccccec4a3e3b8c39f2865962fc20c30990d10afb
Limsungsucf19a <koko1967@naver.com>
2019-04-11 17:32:51 +0900

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,18 +1,19 @@
-# 최대공약수
-def gcd(a, b):
-      while (b != 0):
-        temp = a % b
-        a = b
-        b = temp
-      return abs(a)
+ 
+n = 0
+con = 0
+#list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+list = [1,2,3]
 
-
-# 최소 공배수 
-def lcm(a, b):
-    gcd_value = gcd(a, b)
-    if (gcd_value == 0): 
-        return 0 
-    return abs( int((a * b) / gcd_value) )
-
-print(gcd(12,14))
-print(lcm(12,14))
+while True:
+    n = n + 1
+    for i in list:
+        print(n,i)
+        if n%list[i] != 0:
+            break
+        if list[i] == 3:
+            con = 1
+            break
+    if con == 1:
+        break
+ 
+print(n)
