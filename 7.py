83a197d98377948fe9baa86cc4e95133c406c62d
Limsungsucf19a <koko1967@naver.com>
2019-04-12 15:21:16 +0900

    9

--- 7.py
+++ 7.py
@@ -1,9 +1,8 @@
-
-def getPrimeList(num):
-    primes = []
-    if num < 2:
-        return primes
-    for i in range(2, num+1):
+primes = []
+prime_cnt = 0
+i = 2
+    
+while (prime_cnt < 10001):
         isPrime = True
         for j in primes:
             if i % j == 0:
@@ -12,12 +11,14 @@ def getPrimeList(num):
             elif j > i**0.5:
                 break
         if isPrime:
+            print(prime_cnt)
             primes.append(i)
-    return primes
+            prime_cnt = prime_cnt + 1
+        i = i + 1
+print(primes[10000])      
  
-# 1000 이하의 소수
-if __name__ == '__main__':
-    print(getPrimeList(1000))
+ 
+
 
 
 
