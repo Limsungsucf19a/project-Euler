a220c82a828c91d8365c00f892c08c212737f8a6
Limsungsucf19a <koko1967@naver.com>
2019-05-21 22:46:39 +0900

    Update naver.py

--- naver.py
+++ naver.py
@@ -1,22 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
 
-# 소스코드에서 클래스 찾기 
-# class에
-#  
+import csv
+csvFile = open("../auction.csv", 'w+')
+try :
+    writer = csv.writer(csvFile)
+    writer.writerow(('number','number2','number3'))
+    for i in range(20):
+        writer.writerow((i,i+2,i*2))
+finally:
 
-resp = requests.get('https://www.naver.com/')
+resp = requests.get('http://www.auction.co.kr')
 soup = BeautifulSoup(resp.text, 'html.parser')
 
 hotkeywords = soup.select('.ah_list .ah_item')
 hotnews = soup.select('.ca_item')
 
 for hotkey in hotkeywords:
-    print(f'{hotkey.select_one(".ah_r").get_text()}위 '
+    print(f'{hotkey.select_one(".ah_r").get_text()} '
           f'{hotkey.select_one(".ah_k").get_text()} ')
 
-for news in hotnews:
-    print(f'{news.select_one(".ca_a").get_text()} ')
 
 
 
